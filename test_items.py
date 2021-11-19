54485c1abda32b90dca3409a6ecdfb58eb5911a5
MishutaMaxim <85949443+MishutaMaxim@users.noreply.github.com>
2021-11-19 18:47:34 +0300

    add comments and refactoring

--- test_items.py
+++ test_items.py
@@ -1,14 +1,12 @@
 import time
+# Объявляем адрес страницы
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
+# Запускаем тест
 def test_guest_should_see_login_link(browser):
-    try:
-        # Open link in web driver
-        browser.get(link)
-        # Find a basket button on the page
-        button_basket = browser.find_element_by_class_name('btn-add-to-basket')
-        assert button_basket is not None, 'ERROR: Button basket is NOT found'
-    finally:
-        time.sleep(3)
+    # Открываем ссылку в драйвере
+    browser.get(link)
+    # Находим кнопку по имени класса
+    button_basket = browser.find_element_by_class_name('btn-add-to-basket')
+    # Ассерт на наличие кнопки
+    assert button_basket is not None, 'ERROR: Button basket is NOT found'
