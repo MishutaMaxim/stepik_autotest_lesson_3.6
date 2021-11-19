c225677fee68233eeacd3dac95eaf76c4e17936a
MishutaMaxim <85949443+MishutaMaxim@users.noreply.github.com>
2021-11-19 18:44:50 +0300

    add comments

--- conftest.py
+++ conftest.py
@@ -2,18 +2,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
+# Добавляем опцию с языком для запуска, параметр по умолчанию ru
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="ru",
                      help="Choose language")
 
-
+# Объявляем фикстуру для теста
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("language")
+    user_language = request.config.getoption("language") # Получили пользовательский язык и записали в переменную
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language}) # Передали опцию с языком браузера
+    browser = webdriver.Chrome(options=options) # Объявили сам браузер
     yield browser
-    print("\nquit browser..")
-    browser.quit()
+    browser.quit() # Закрыли браузер после теста
